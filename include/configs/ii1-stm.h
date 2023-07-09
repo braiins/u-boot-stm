545cbb11a48aa7c34a4a0c0f0958c64be584695f
Libor Vašíček <libor.vasicek@braiins.cz>
2023-07-09 21:55:54 +0200

    ii1-stm: Create default boot script for Braiins board #BOS-1357
    - remove 'CONFIG_BOOTARGS' from default configuration
    - remove 'splashimage' from 'CONFIG_EXTRA_ENV_SETTINGS'
    - remove all 'BOOT_TARGET_*' definitions
    - add 'bootcmd_bos' definition to default env
    - set 'CONFIG_BOOTCOMMAND' to 'run bootcmd_bos'

--- include/configs/ii1-stm.h
+++ include/configs/ii1-stm.h
@@ -1,8 +1,29 @@
 /* SPDX-License-Identifier: GPL-2.0+ OR BSD-3-Clause */
 /*
- * Copyright (C) 2018, STMicroelectronics - All Rights Reserved
+ * Copyright (C) 2023  Braiins Systems s.r.o.
  *
- * Configuration settings for the STM32MP15x CPU
+ * This file is part of Braiins Open-Source Initiative (BOSI).
+ *
+ * BOSI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Please, keep in mind that we may also license BOSI or any part thereof
+ * under a proprietary license. For more information on the terms and conditions
+ * of such proprietary license or if you have any other questions, please
+ * contact us at opensource@braiins.com.
+ */
+/*
+ * Copyright (C) 2018, STMicroelectronics - All Rights Reserved
  */
 
 #ifndef __CONFIG_H
@@ -63,64 +84,29 @@
 
 #if !defined(CONFIG_SPL_BUILD)
 
-#ifdef CONFIG_CMD_MMC
-#define BOOT_TARGET_MMC0(func)	func(MMC, mmc, 0)
-#define BOOT_TARGET_MMC1(func)	func(MMC, mmc, 1)
-#define BOOT_TARGET_MMC2(func)	func(MMC, mmc, 2)
-#else
-#define BOOT_TARGET_MMC0(func)
-#define BOOT_TARGET_MMC1(func)
-#define BOOT_TARGET_MMC2(func)
-#endif
+#define SET_ROOTFS_PART "set_rootfs_part=" \
+	"part number mmc $boot_instance rootfs${rootfs_index} rootfs_part && " \
+	"setexpr rootfs_part dec $rootfs_part && " \
+	"\0"
 
-#ifdef CONFIG_NET
-#define BOOT_TARGET_PXE(func)	func(PXE, pxe, na)
-#else
-#define BOOT_TARGET_PXE(func)
-#endif
+#define SET_KERNEL_ROOTFS "set_kernel_rootfs=" \
+	"setenv kernel_rootfs /dev/mmcblk${boot_instance}p${rootfs_part}" \
+	"\0"
 
-#ifdef CONFIG_CMD_UBIFS
-#define BOOT_TARGET_UBIFS(func)	func(UBIFS, ubifs, 0)
-#else
-#define BOOT_TARGET_UBIFS(func)
-#endif
+#define SET_BOOTARGS "set_bootargs=" \
+	"setenv bootargs root=${kernel_rootfs} rootwait rw earlyprintk console=ttySTM0,115200" \
+	"\0"
 
-#define BOOT_TARGET_DEVICES(func)	\
-	BOOT_TARGET_MMC1(func)		\
-	BOOT_TARGET_UBIFS(func)		\
-	BOOT_TARGET_MMC0(func)		\
-	BOOT_TARGET_MMC2(func)		\
-	BOOT_TARGET_PXE(func)
-
-/*
- * bootcmd for stm32mp1:
- * for serial/usb: execute the stm32prog command
- * for mmc boot (eMMC, SD card), boot only on the same device
- * for nand or spi-nand boot, boot with on ubifs partition on UBI partition
- * for nor boot, use SD card = mmc0
- */
-#define STM32MP_BOOTCMD "bootcmd_stm32mp=" \
-	"echo \"Boot over ${boot_device}${boot_instance}!\";" \
-	"if test ${boot_device} = serial || test ${boot_device} = usb;" \
-	"then stm32prog ${boot_device} ${boot_instance}; " \
-	"else " \
-		"run env_check;" \
-		"if test ${boot_device} = mmc;" \
-		"then env set boot_targets \"mmc${boot_instance}\"; fi;" \
-		"if test ${boot_device} = nand ||" \
-		  " test ${boot_device} = spi-nand ;" \
-		"then env set boot_targets ubifs0; fi;" \
-		"if test ${boot_device} = nor;" \
-		"then env set boot_targets mmc0; fi;" \
-		"run distro_bootcmd;" \
-	"fi;\0"
-
-#include <config_distro_bootcmd.h>
+#define BOOTCMD_BOS "bootcmd_bos=" \
+	"run set_rootfs_part set_kernel_rootfs set_bootargs && " \
+	"load mmc ${boot_instance}:${rootfs_part} $kernel_addr_r boot/zImage && " \
+	"load mmc ${boot_instance}:${rootfs_part} $fdt_addr_r boot/stm32mp157c-ii1.dtb && " \
+	"bootz $kernel_addr_r - $fdt_addr_r" \
+	"\0"
 
 /*
  * memory layout for 32M uncompressed/compressed kernel,
- * 1M fdt, 1M script, 1M pxe and 1M for splashimage
- * and the ramdisk at the end.
+ * 1M fdt, 1M script, 1M pxe and the ramdisk at the end.
  */
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"kernel_addr_r=0xc2000000\0" \
@@ -128,13 +114,11 @@
 	"fdtoverlay_addr_r=0xc4100000\0" \
 	"scriptaddr=0xc4100000\0" \
 	"pxefile_addr_r=0xc4200000\0" \
-	"splashimage=0xc4300000\0"  \
 	"ramdisk_addr_r=0xc4400000\0" \
-	"altbootcmd=run bootcmd\0" \
-	"env_check=if env info -p -d -q; then env save; fi\0" \
-	STM32MP_BOOTCMD \
-	BOOTENV \
-	"boot_net_usb_start=true\0"
+	SET_ROOTFS_PART \
+	SET_KERNEL_ROOTFS \
+	SET_BOOTARGS \
+	BOOTCMD_BOS
 
 #endif /* ifndef CONFIG_SPL_BUILD */
 #endif /* ifdef CONFIG_DISTRO_DEFAULTS*/
